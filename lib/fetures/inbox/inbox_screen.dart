8be22a847833f286722d36892b105c5af5d09523
김다은 <150661115+dani1552@users.noreply.github.com>
2024-01-04 16:42:41 +0900

    #10-1

---
+++ lib/fetures/inbox/inbox_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/fetures/inbox/activity_screen.dart';
+
+class InBoxScreen extends StatelessWidget {
+  const InBoxScreen({super.key});
+
+  void _onDmPressed() {}
+  void _onActivityTap(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const ActivityScreen(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 1,
+        title: const Text("Inbox"),
+        actions: [
+          IconButton(
+            onPressed: _onDmPressed,
+            icon: const FaIcon(FontAwesomeIcons.paperPlane),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            onTap: () => _onActivityTap(context),
+            title: const Text(
+              "Activity",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: Sizes.size16,
+              ),
+            ),
+            trailing: const FaIcon(
+              FontAwesomeIcons.chevronRight,
+              size: Sizes.size14,
+              color: Colors.black,
+            ),
+          ),
+          Container(
+            height: Sizes.size1,
+            color: Colors.grey.shade200,
+          ),
+          ListTile(
+            leading: Container(
+              width: Sizes.size52,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.blue,
+              ),
+              child: const Center(
+                child: FaIcon(
+                  FontAwesomeIcons.users,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            title: const Text(
+              "New Followers",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: Sizes.size16,
+              ),
+            ),
+            subtitle: const Text("Messages from followers will appear here",
+                style: TextStyle(
+                  fontSize: Sizes.size14,
+                )),
+            trailing: const FaIcon(
+              FontAwesomeIcons.chevronRight,
+              size: Sizes.size14,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
