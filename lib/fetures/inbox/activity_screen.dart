10d61c9a7027a2d5dfada170a26917fb0ed63044
김다은 <150661115+dani1552@users.noreply.github.com>
2024-01-08 13:42:25 +0900

    #10-4

--- lib/fetures/inbox/activity_screen.dart
+++ lib/fetures/inbox/activity_screen.dart
@@ -15,37 +15,52 @@ class _ActivityScreenState extends State<ActivityScreen>
     with SingleTickerProviderStateMixin {
   final List<String> _notifications = List.generate(20, (index) => "${index}h");
 
-  late final AnimationController _aninationController =
-      AnimationController(vsync: this);
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: const Duration(
+      milliseconds: 300,
+    ),
+  );
 
   late final Animation<double> _animation =
-      Tween(begin: 0.0, end: 1.0).animate(_aninationController);
+      Tween(begin: 0.0, end: 0.5).animate(_animationController);
 
   void _onDismissed(String notification) {
     _notifications.remove(notification);
     setState(() {});
   }
 
+  void _onTitleTap() {
+    if (_animationController.isCompleted) {
+      _animationController.reverse();
+    } else {
+      _animationController.forward();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          centerTitle: true,
-          title: const Row(
+        centerTitle: true,
+        title: GestureDetector(
+          onTap: _onTitleTap,
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("All activity"),
-          Gaps.h2,
+              const Text("All activity"),
+              Gaps.h2,
+              RotationTransition(
+                turns: _animation,
+                child: const FaIcon(
+                  FontAwesomeIcons.chevronDown,
+                  size: Sizes.size14,
+                ),
+              ),
             ],
           ),
-          RotationTransition(
-            turns: _animation, 
-            ,
-            child: const FaIcon(
-              FontAwesomeIcons.chevronDown,
-              size: Sizes.size14,
-            ),
-          )),
+        ),
+      ),
       body: ListView(
         children: [
           Gaps.v14,
